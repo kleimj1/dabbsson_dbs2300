7fded6294da7b80d0d5aef28917f117b570efd96
kleimj1 <jochen.kleimann@gmail.com>
2025-04-05 10:02:05 +0200

    Update switch.py

--- custom_components/dabbsson_dbs2300/switch.py
+++ custom_components/dabbsson_dbs2300/switch.py
@@ -1,13 +1,15 @@
+# custom_components/dabbsson_dbs2300/switch.py
 
 from homeassistant.components.switch import SwitchEntity
-from .const import DOMAIN, BLE_KEYS
+from homeassistant.config_entries import ConfigEntry
+from .const import DOMAIN
 
-async def async_setup_entry(hass, config_entry, async_add_entities):
-    client = hass.data[DOMAIN][config_entry.entry_id]
+async def async_setup_entry(hass, config_entry: ConfigEntry, async_add_entities):
+    client = hass.data[DOMAIN][config_entry.entry_id]["client"]
     switches = [
-        DBSwitch(client, "AC Output", BLE_KEYS["ac_output"]),
-        DBSwitch(client, "5V Output", BLE_KEYS["out_5v"]),
-        DBSwitch(client, "12V Output", BLE_KEYS["out_12v"]),
+        DBSwitch(client, "AC Output", 109),
+        DBSwitch(client, "5V Output", 111),
+        DBSwitch(client, "12V Output", 112),
     ]
     async_add_entities(switches, True)
 
