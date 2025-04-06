1eaa9c11acd57974970fe419fe13d2233df3e52f
kleimj1 <jochen.kleimann@gmail.com>
2025-04-06 20:50:22 +0200

    Update config_flow.py

--- custom_components/dabbsson_dbs2300/config_flow.py
+++ custom_components/dabbsson_dbs2300/config_flow.py
@@ -1,18 +1,25 @@
-
-from homeassistant import config_entries
 import voluptuous as vol
+from homeassistant import config_entries
+from homeassistant.const import CONF_NAME
+from .const import DOMAIN, CONF_MQTT_TOPIC
 
-class DabbssonFlowHandler(config_entries.ConfigFlow, domain="dabbsson_dbs2300"):
+class DabbssonConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    VERSION = 1
     async def async_step_user(self, user_input=None):
         errors = {}
         if user_input is not None:
-            return self.async_create_entry(title="Dabbsson DBS2300", data=user_input)
+            return self.async_create_entry(
+                title=user_input[CONF_NAME],
+                data=user_input,
+            )
 
         data_schema = vol.Schema({
-            vol.Required("ble_address"): str
+            vol.Required(CONF_NAME): str,
+            vol.Required(CONF_MQTT_TOPIC): str,
         })
-        return self.async_show_form(step_id="user", data_schema=data_schema, errors=errors)
 
-# Hinweis:
-# parse_ble_data ist eine Hilfsfunktion, die du basierend auf der BLE-Datenstruktur implementierst.
-# Die Schlüssel müssen entsprechend der BLE-GATT-Spezifikation des DBS2300 zugeordnet sein.
+        return self.async_show_form(
+            step_id="user",
+            data_schema=data_schema,
+            errors=errors
+        )
