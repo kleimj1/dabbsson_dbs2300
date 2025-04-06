99d6b4d201108005aded7d1951a1ec4c089bd83b
kleimj1 <jochen.kleimann@gmail.com>
2025-04-06 20:49:34 +0200

    Update __init__.py

--- custom_components/dabbsson_dbs2300/__init__.py
+++ custom_components/dabbsson_dbs2300/__init__.py
@@ -1,44 +1,39 @@
-# custom_components/dabbsson_dbs2300/__init__.py
+## Datei: __init__.py
 
 import logging
-from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant
-from bleak import BleakClient
+import voluptuous as vol
+import homeassistant.helpers.config_validation as cv
 
-from .const import DOMAIN
+from homeassistant.const import CONF_NAME
+from homeassistant.core import callback
+from homeassistant.helpers.discovery import async_load_platform
 
-PLATFORMS = ["sensor", "switch"]
+DOMAIN = "dabbsson_dbs2300"
+CONF_DEVICES = "devices"
+CONF_MQTT_TOPIC = "mqtt_topic"
+
 _LOGGER = logging.getLogger(__name__)
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Set up Dabbsson DBS2300 from a config entry."""
-    ble_address = entry.data["ble_address"]
-    client = BleakClient(ble_address)
+CONFIG_SCHEMA = vol.Schema(
+    {
+        DOMAIN: vol.Schema(
+            {
+                vol.Required(CONF_DEVICES): [
+                    {
+                        vol.Required(CONF_NAME): cv.string,
+                        vol.Required(CONF_MQTT_TOPIC): cv.string,
+                    }
+                ]
+            }
+        )
+    },
+    extra=vol.ALLOW_EXTRA,
+)
 
-    try:
-        await client.connect()
-        _LOGGER.info("Connected to DBS2300 at %s", ble_address)
-    except Exception as e:
-        _LOGGER.error("Could not connect to DBS2300 at %s: %s", ble_address, e)
-        return False
-
-    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = {
-        "client": client,
-        "address": ble_address,
-    }
-
-    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+async def async_setup(hass, config):
+    devices = config[DOMAIN][CONF_DEVICES]
+    for device in devices:
+        hass.async_create_task(
+            async_load_platform(hass, "sensor", DOMAIN, device, config)
+        )
     return True
-
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Unload a config entry."""
-    client = hass.data[DOMAIN][entry.entry_id]["client"]
-    try:
-        await client.disconnect()
-    except Exception as e:
-        _LOGGER.warning("Error disconnecting from DBS2300: %s", e)
-
-    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
-    if unload_ok:
-        hass.data[DOMAIN].pop(entry.entry_id)
-    return unload_ok
